8d9306025f6b596a39a45081a11bae5cf3c558c3
Mst.Shorifa <shorifaakter825@gmail.com>
2023-08-29 23:08:05 +0600

    updated

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/super-admin-dashboard',[SuperAdminController::class,'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
